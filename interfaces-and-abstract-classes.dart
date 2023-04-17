b3c9cbbadc0bde92796a74c7b09a8776f2231657
Wang Yunfeng <70517424+LittlePrince404@users.noreply.github.com>
2023-04-17 03:01:20 +0000

    rename

---
+++ interfaces-and-abstract-classes.dart
@@ -0,0 +1,15 @@
+import 'classes.dart';
+
+class MockSpaceship implements Spacecraft{
+  // ...
+}
+
+abstract class Describable{
+  void describe();
+
+  void describeWithEmphasis(){
+    print('=========');
+    describe();
+    print('=========');
+  }
+}
